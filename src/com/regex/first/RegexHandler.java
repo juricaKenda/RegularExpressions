b8c5650658e5e0a70a1e4ef497b43052f983cc13
Jurica Kenda <jurica.kenda@gmail.com>
2019-03-17 00:01:42 +0100

    Files added

---
+++ src/com/regex/first/RegexHandler.java
@@ -0,0 +1,46 @@
+package com.regex.first;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexHandler {
+
+	private String inputString;
+	private Pattern regexPattern;
+	private Matcher matcher;
+	public static final String mailTracker = "(\\w{1,})([. -]?)(\\w{0,})[@]([A-Za-z]{1,})(\\.)(\\w{1,})";
+	public static final String titleNameTracker = "((Mr)|(mr))(s?)(.?)(\\s?)([A-Za-z]{0,})(\\s?)([A-z]{0,})";
+	
+	
+	public RegexHandler(File file) {
+		this.inputString = this.extractFromFile(file);
+	}
+	
+	
+	public void findPattern(String chosenPattern) {
+		System.out.println("-------------------");
+		this.regexPattern = Pattern.compile(chosenPattern);
+		this.matcher = this.regexPattern.matcher(inputString);
+		
+		while(matcher.find()) {
+			System.out.println(matcher.group());
+		}
+	}
+	
+	
+	public static String extractFromFile(File file) {
+		StringBuffer buffer = new StringBuffer();
+		try(Scanner input = new Scanner(file)){
+			while(input.hasNext()) {
+				buffer.append(input.nextLine()+"\n");
+			}
+			
+		}catch(IOException e) {
+			
+		}
+	return buffer.toString();
+	}
+}
---
+++ src/com/regex/first/RegexMain.java
@@ -0,0 +1,14 @@
+package com.regex.first;
+
+import java.io.File;
+
+public class RegexMain {
+
+	
+	public static void main(String[] args) {
+		RegexHandler handler = new RegexHandler(new File("src\\regex.txt"));
+		handler.findPattern(RegexHandler.titleNameTracker);
+		handler.findPattern(RegexHandler.mailTracker);
+		
+	}
+}
